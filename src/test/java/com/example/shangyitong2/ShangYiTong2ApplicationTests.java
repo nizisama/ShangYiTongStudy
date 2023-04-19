6655bba04f136c773404809cca6b74c368c52739
nizisama <1072511563@qq.com>
2023-04-19 16:13:32 +0800

    first

---
+++ src/test/java/com/example/shangyitong2/ShangYiTong2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shangyitong2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShangYiTong2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
